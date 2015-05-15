a2061bf61d673e98cc34033080fffd8a0dbffc38
Norbert Bogusiewicz <nbbogus7@gmail.com>
2015-05-16 00:26:44 +0200

    - add view model - change folder hierarchy - add search

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,5 @@
 /// <reference path="../../bower_components/dt-angular/angular.d.ts" />
+/// <reference path="../../bower_components/dt-jquery/jquery.d.ts" />
 ///<reference path="main\main.controller.ts"/>
 'use strict';
 var projectManagement;
